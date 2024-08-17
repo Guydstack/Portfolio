8e6e885998f992a52e95a8ff21d290ed03b92906
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-08-17 23:07:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react'
+import { mode } from "@chakra-ui/theme-tools";
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
      <App />
     </ChakraProvider>
   </React.StrictMode>,
